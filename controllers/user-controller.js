8b97475308177d59940f73cc5e20d958b9f2b41e
Ben <benjamind10@pm.me>
2022-04-05 10:22:54 -0400

    added find by id and update user controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -24,6 +24,8 @@ const userController = {
         res.status(400).json(err);
       });
   },
+
+  // Creates a user
   createUser({ body }, res) {
     User.create(body)
       .then(dbUserData => res.json(dbUserData))
@@ -31,6 +33,48 @@ const userController = {
         console.log(err);
         res.status(400).json(err);
       });
+  },
+
+  // Searches for user by ID
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v',
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v',
+        strictPopulate: false,
+      })
+      .select('-__v')
+      .then(dbUserData => {
+        !dbUserData
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that id.' })
+          : res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // Updates a user info
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then(dbUserData => {
+        !dbUserData
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that id.' })
+          : res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
   },
 };
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 const {
   getAllUsers,
   createUser,
+  getUserById,
 } = require('../../controllers/user-controller');
 
 router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:id').get(getUserById);
 
 module.exports = router;
